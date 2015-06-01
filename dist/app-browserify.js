f7525c1b9b25cdc2d9962c8d32ce61f4f31cbf79
paulesaad <paulsaad.13@gmail.com>
2015-06-01 09:44:02 -0500

    first commit

--- dist/app-browserify.js
+++ dist/app-browserify.js
@@ -1,1 +1,1 @@
-
+console.error("SyntaxError: /Users/paulsaad/Github Projects/github-profile/js/app-browserify.js: Unexpected token (34:71) while parsing file: /Users/paulsaad/Github Projects/github-profile/js/app-browserify.js");
--- js/app-browserify.js
+++ js/app-browserify.js
@@ -4,21 +4,45 @@ var Promise = require('es6-promise').Promise
 // just Node?
 // var fetch = require('node-fetch')
 // Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
+require('whatwg-fetch') //--> not a typo, don't store as a var
 
 // es6 polyfills, powered by babel
 require("babel/register")
 
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
+var urls = [ 'https://api.github.com/users/paulesaad','https://api.github.com/users/paulesaad/repos' ]
 
-// require your own libraries, too!
-// var Router = require('./app.js')
+var promises = urls.map((url) => fetch(url).then((r) => r.json()))
 
-// window.addEventListener('load', app)
 
-// function app() {
-    // start app
-    // new Router()
-// }
+//Creates array with JSON of profile information in 0th index and repo info in 1st index
+var sort = Promise.all(promises).then((data_array) => {
+    var profile_info = data_array[0]
+    var repo_info = data_array[1]
+})
 
+//Creates profComps with array of objects including profile info & repoNames with an array of repo names.
+var sorted = sort.then((data_array) => {
+	var neededComps = [name, login, blog, location, email, html_url]
+	var profComps = neededComps.map((v) => `${v} : ${profile_info.v}`)
+	var repoNames = repo_info.map((v) => v.name)
+	console.log(profComps)
+	console.log(repoNames)
+})
+
+var prof_listify = (arr) => {
+	arr.forEach((v) =>
+	document.querySelector('.infolist').innerHTML = `<li>${Object.keys(v) : ${v[Object.keys(v)]}}</li>`
+	)
+}
+
+var repos_listify = (arr) => {
+	array.forEach((v) =>
+	document.querySelector('.repolist').innerHTML = `<li>${v}</li>`
+	)
+}
+
+sorted.then(() => {
+	document.querySelector('.profile').innerHTML = `<img src=${profile_info.avatar_url}>`
+	prof_listify(profComps)
+	repos_listify(repoNames)
+}

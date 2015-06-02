0eb13716d2b722c3dba9f8a49aced10244b42a3c
paulesaad <paulsaad.13@gmail.com>
2015-06-02 11:40:07 -0500

    Created constructor function and placed getData method within its  prototype

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -1,30 +1,54 @@
 "use strict";
 
 var Promise = require('es6-promise').Promise
-// just Node?
-// var fetch = require('node-fetch')
-// Browserify?
 require('whatwg-fetch') //--> not a typo, don't store as a var
 
 // es6 polyfills, powered by babel
 require("babel/register")
 
-var urls = [ 'https://api.github.com/users/paulesaad','https://api.github.com/users/paulesaad/repos' ]
-
-var requests = urls.map((url) => fetch(url).then((r) => r.json()))
-
 function qs(selector) {
-    return document.querySelector(selector)
+       return document.querySelector(selector)
 }
 
-Promise.all(requests).then((data) => {
-    var profile = data[0],
-        repos = data[1]
 
-    var profile_string = ['name', 'login', 'blog', 'location', 'email', 'html_url'].map((key) => `<li>${key}: ${profile[key]}</li>`).join('')
-    var repo_string = repos.map((repo) => `<li><a href="${repo.html_url}">${repo.name}</a></li>`).join('')
+//-------------------------------------------------------------------
+//DEFINITION CODE
 
-    qs('.profile img').src = profile.avatar_url
-    qs('.profile ul').innerHTML = profile_string
-    qs('.repos ul').innerHTML = repo_string
-})
+//(object) Constructor function
+function GithubClient(loginname){
+    this.username = loginname
+}
+
+//prototype methods/functions need names!  `GHClient.prototype.doSomething =` NOT `ghClient.prototype =`
+GithubClient.prototype.getData = function(username){
+    var token = "1654ffd6cec30f98ef118dd04c758bf3b9dde323"
+
+    var urls = [
+    	`https://api.github.com/users/${this.username}?access_token=${token}`, 
+    	`https://api.github.com/users/${this.username}/repos?access_token=${token}`
+    ]
+
+    var requests = urls.map((url) => fetch(url).then((r) => r.json()))
+   
+    Promise.all(requests).then((data) => {
+        var profile = data[0],
+            repos = data[1]
+
+        var profile_string = ['name', 'login', 'blog', 'location', 'email', 'html_url'].map((key) => `<li>${profile[key]}</li>`).join('')
+        var repo_string = repos.map((repo) => `<li><a href="${repo.html_url}">${repo.name}</a></li>`).join('')
+
+        qs('.profile img').src = profile.avatar_url
+        qs('.profile ul').innerHTML = profile_string
+        qs('.repos ul').innerHTML = repo_string
+    })
+}
+
+
+//----------------------------------
+//EXECUTION CODE
+var paulSaadClient = new GithubClient('paulesaad')
+var travisClient = new GithubClient('t3patterson')
+
+
+paulSaadClient.getData();
+
---
+++ js/challenges0602.js
@@ -0,0 +1,34 @@
+// The function below allows you to call another
+// function at a later time. It takes 2 params, an
+// amount of miliseconds and a function. It will
+// call the function that many milliseconds later.
+// Refactor it so that is has a default timeout.
+// 
+// This is more advanced than the default param on
+// the accelerate function. This is because there
+// is another parameter to consider.
+// 
+// When setting the timeout, the function needs to
+// work like this:
+// 
+//     callLater(1000, function(){
+//          ...
+//     })
+//
+// When using the default timeout, the function
+// needs to work like this:
+//
+//     callLater(function(){
+//          ...
+//     })
+
+function callLater(timeout, callback) {
+	
+
+	callback = callback || timeout
+	timeout = timeout insanceof Function ? 2000 : timeout
+	setTimeout(callback, timeout)
+}
+
+callLater(1000, () => console.log('hi!'))
+callLater(() => console.log('hi!'))
